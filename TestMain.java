16b15d78dea4598d919819bca32fca969a7a0aa7
opengrafity <dev@grafity.dev>
2023-07-06 19:31:36 +0530

    b2 changes

--- TestMain.java
+++ TestMain.java
@@ -2,5 +2,6 @@ public class Main{
 	publiv static void main(String[] args){
 		System.out.println("Vandhematharam Vandhematharam");
 		System.out.println("Jai Hindh");
+		System.out.println("Mera Bharath Mahan");
 	}
 }	
